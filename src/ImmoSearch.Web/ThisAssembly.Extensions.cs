64fdc4410f235bedc27eead590d1492619900bdb
paralax <paralax@speckdrumm.org>
2026-01-06 00:59:59 +0100

    Make developer info more verbose + easier to read

--- src/ImmoSearch.Web/ThisAssembly.Extensions.cs
+++ src/ImmoSearch.Web/ThisAssembly.Extensions.cs
@@ -1,30 +1,30 @@
-using System;
 using ImmoSearch.Domain.Extensions;
+#pragma warning disable IDE0130
 
 partial class ThisAssembly
 {
     /******************************************************************************************
      * FIELDS
      * ***************************************************************************************/
-    static string? _gitCommitUrl;
+    static string? GitCommitUrlFld;
 
     /******************************************************************************************
      * PROPERTIES
      * ***************************************************************************************/
-    public static string GitCommitUrl => _gitCommitUrl ??= GetGitCommitUrl();
+    public static string GitCommitUrl => GitCommitUrlFld ??= GetGitCommitUrl();
 
-    public static string AssemblyShortFileVersion => Version.Parse(AssemblyFileVersion).ToString(3);
+    public static string AssemblyShortFileVersion => Version.Parse(global::ThisAssembly.AssemblyFileVersion).ToString(3);
 
     /******************************************************************************************
      * METHODS
      * ***************************************************************************************/
     static string GetGitCommitUrl()
     {
-        if (GitRepositoryUrl.NullOrWhitespace || GitCommitId.NullOrWhitespace) return string.Empty;
+        if (global::ThisAssembly.GitRepositoryUrl.NullOrWhitespace || global::ThisAssembly.GitCommitId.NullOrWhitespace) return string.Empty;
 
-        var gitIdx = GitRepositoryUrl.LastIndexOf(".git", StringComparison.InvariantCulture);
-        var repoUrlBase = gitIdx > 0 ? GitRepositoryUrl[..gitIdx] : GitRepositoryUrl.TrimEnd('/');
-        return $"{repoUrlBase}/commit/{GitCommitId}";
+        var gitIdx = global::ThisAssembly.GitRepositoryUrl.LastIndexOf(".git", StringComparison.InvariantCulture);
+        var repoUrlBase = gitIdx > 0 ? global::ThisAssembly.GitRepositoryUrl[..gitIdx] : global::ThisAssembly.GitRepositoryUrl.TrimEnd('/');
+        return $"{repoUrlBase}/commit/{ThisAssembly.GitCommitId}";
     }
 
 }
